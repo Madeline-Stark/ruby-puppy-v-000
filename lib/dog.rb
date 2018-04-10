b6b0a335a7c837f1d608667212947d065078a33a
Madeline Stark <madeline.stark1@gmail.com>
2018-04-10 20:33:51 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,11 +4,17 @@ attr_accessor :name
 @@all = []
 
 def initialize(name)
-  @name = name 
+  @name = name
+  @@all << self
+end
 
+def self.all?
+  @@all.each do |puppy|
+      puts puppy.name
+    end
 end
 
 
 
 
-end 
+end
